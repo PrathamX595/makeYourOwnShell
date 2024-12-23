562c55d9c7631dd439712775e50b19ce6c8429cd
PrathamX595 <pratham.seth.cer23@itbhu.ac.in>
2024-12-23 21:30:40 +0530

    echo builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,27 +4,30 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
 	finished := false
-	
+
 	// Wait for user input
-	for !finished{
+	for !finished {
 		fmt.Fprint(os.Stdout, "$ ")
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
-		command = strings.TrimSpace(command)
-		if(command == "exit 0"){
+		input = strings.TrimSpace(input)
+		splitcomm := strings.Split(input, " ")
+		command := splitcomm[0]
+		if command == "exit" {
 			finished = true
-			os.Exit(0)
+			code, _ := strconv.ParseInt(splitcomm[1], 10, 64)
+			os.Exit(int(code))
+		}else if command == "echo"{
+			fmt.Println(strings.Join(splitcomm[1:], " "))
 		}
-		command = command + ": command not found"
-		fmt.Println(command)
 	}
-
 }
